aef5317b7e12ee44497b41150429b045ed73e273
An Vu <anvu94@gmail.com>
2015-06-18 18:07:13 -0400

    got it up and working! just need to understand xeditable and links

--- js/app.js
+++
@@ -1,1 +0,0 @@
-var newsletter = angular.module('newsApp',['ngSanitize','xeditable']);
--- js/newsletter.js
+++ js/newsletter.js
@@ -1,25 +1,50 @@
 //Angular!
+var newsletter = angular.module('newsApp',['ngSanitize','xeditable']);
+
 newsletter.run(function(editableOptions) {
       editableOptions.theme = 'default';
-      });
+});
 
 newsletter.controller('newsController',function($scope){
 	
-	$scope.sections =  {
-		section: "Faculty",
-		articles: [
+	$scope.news =  {
+		sections: [
 			{
-				title: "In Memoriam: Eli Pearce",
-				link:'http://engineering.nyu.edu/news/2015/05/29/memoriam-eli-pearce',
-				publisher: "NYU Newsroom",
-				orig_title:"   ",
-				description: "When <strong>Professor Eli Pearce</strong> passed away on May 19, 2015, a slice of Poly history passed along with him. Pearce had been affiliated with the school, then known as the Polytechnic Institute of Brooklyn, since the mid-1950s, when he conducted his doctoral studies in chemistry here. As a student, he learned with such luminaries as Herman Frances Mark, who is often called the Father of Polymer Science, and Charles Overberger, another influential chemist who helped establish the study of polymers as a major sub-discipline."
+				title: "Faculty",
+				articles: [
+					{
+						title: "In Memoriam: Eli Pearce",
+						link:'http://engineering.nyu.edu/news/2015/05/29/memoriam-eli-pearce',
+						publisher: "NYU Newsroom",
+						orig_title:"   ",
+						description: "When <strong>Professor Eli Pearce</strong> passed away on May 19, 2015, a slice of Poly history passed along with him. Pearce had been affiliated with the school, then known as the Polytechnic Institute of Brooklyn, since the mid-1950s, when he conducted his doctoral studies in chemistry here. As a student, he learned with such luminaries as Herman Frances Mark, who is often called the Father of Polymer Science, and Charles Overberger, another influential chemist who helped establish the study of polymers as a major sub-discipline."
+					}
+				]
 			}
 		]
 	};
-	$scope.addItem = function(){
-		$scope.sections.articles.push(this.sections.articles.temp);
-		$scope.sections.articles.temp={};
+	
+	//Add Section
+	$scope.addSection = function(){
+		$scope.news.sections.temp={
+			title: "Section Title",
+			articles:[
+				{
+					title:"Article Title",
+					link:"Link",
+					publisher: "Publisher",
+					orig_title: "Original Title",
+					description: "Description"
+				}
+			]
+		};
+		$scope.news.sections.push(this.news.sections.temp);
+	};
+	
+	$scope.addArticle = function(index){
+		console.log(index);
+		$scope.news.sections[index].articles.push($scope.news.sections[index].articles.temp);
+		$scope.news.sections[index].articles.temp={};
 	};
 	
 });
@@ -27,13 +52,6 @@ newsletter.controller('newsController',function($scope){
 
 
 // Menu bar
-
-//Add Section
-var newSection = '<div class = "section" ng-controller="newsController"><h2 id="section" editable-text="sections.section">{{sections.section}}</h2><div class = "article" ng-repeat="article in sections.articles"><h3 id="title" editable-text="article.title"><a editable-text="article.link" href="{{article.link}}">{{article.title}}</a></h3><p id="publisher" editable-text="article.publisher">{{article.publisher}}</p><p id="orig_title" editable-text="article.orig_title">{{article.orig_title}}</p><p id="descr" ng-bind-html="article.description" editable-text="article.description"></p></div><div ng-mouseenter="showEditor=true" ng-mouseleave="showEditor=false" class="article editor"><div ng-show="showEditor"><div><h3>Add article</h3><div><form role="form" ng-submit="addArticle()"><div><label for="articleName">Article Name</label><div><input type="text" ng-model="sections.articles.temp.title" placeholder="Faculty News"></div></div><div><label for="articleLink"> Article Link</label><div><input type="text"ng-model="sections.articles.temp.link" placeholder="htt://engineering.nyu.edu"></div></div><div><label for="articlePub"> Publisher </label><div><input type="text"ng-model="sections.articles.temp.publisher" placeholder="NYU"></div></div><div><label for="articleOrgTitle"> Original Title</label><div><input type="text"ng-model="sections.articles.temp.orig_title" placeholder="Top 5"></div></div><div><label for="articleDescr"> Description</label><div><input type="text"ng-model="sections.articles.temp.description" placeholder="We are number 1!"></div></div><div><div><button type="submit">Add</button></div></div></form></div></div></div></div></div>'
-$("#add-section").click(function(){
-	var $section = $('#main').append(newSection);
-});
-
 
 //Select All
 
@@ -53,10 +71,5 @@ function selectText(element){
 
 }
 
-// //Send to Server
-// 	$("#main").ready(function() { 
-// 	    $.getJSON( $("#json").html() , function(json) {
-// 	        $.each(json.data, function(index, value) { $('.data').append(value.a+'<br />'); } );
-// 	    });
-// 	});
-// ;
+//Send to Server
+
