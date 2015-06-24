2dc907acfcebca62dfe22e9a07571b4fe638ef40
An Vu <anvu94@gmail.com>
2015-06-24 14:05:41 -0400

    tweaking the description label

--- js/newsletter.js
+++ js/newsletter.js
@@ -81,12 +81,13 @@ function selectText(element){
        var range = document.body.createTextRange();
        range.moveToElementText( node  );
        range.select();
+   		
    } 
    	else if ( window.getSelection ) {
        var range = document.createRange();
        range.selectNodeContents( node );
        window.getSelection().removeAllRanges();
        window.getSelection().addRange( range );
+	   console.log('SELECTED!');
    	}
-
 }
